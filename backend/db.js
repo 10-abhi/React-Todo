42a5bb78c222266c047d87abc247c7766cad0398
Abhishek Mishra <abhishek81936@gmail.com>
2024-08-16 11:16:13 +0530

    updating

--- backend/db.js
+++ backend/db.js
@@ -2,9 +2,8 @@ const mongoose = require('mongoose');
 
 const { strictObject } = require('zod');
 
- mongoose.connect("mongodb+srv://abhigdscnew:abhi8193681936@cluster0.v3kjunr.mongodb.net/")
-
-// will not do like this in professional project , will put this in .env file and you will not push this in github
+ mongoose.connect("PutconnectionStringHere")
+  
 const todoSchema = mongoose.Schema({
     title : String ,
     description : String ,
